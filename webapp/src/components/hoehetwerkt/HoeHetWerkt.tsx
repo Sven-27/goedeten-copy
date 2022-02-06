18010fb19842bcc1bc969ead2ccb417214188bba
Sven-27 <sven.notermans@live.nl>
2022-02-06 10:39:05 +0100

    deleted svg and made it easier

--- webapp/src/components/hoehetwerkt/HoeHetWerkt.tsx
+++ webapp/src/components/hoehetwerkt/HoeHetWerkt.tsx
@@ -1,13 +1,22 @@
 import styles from "styles/customer/hoehetwerkt/HoeHetWerkt.module.scss"
-import Svg from "./Svg"
+import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
 
+const Cloche = "/assets/images/cloche.png"
+ 
 const HoeHetWerkt = () => {
   return (
     <main className={styles.hoehetwerkt}>
       <section className={styles.ontvangen}>
         <h1>Kies de dag waarop je het wilt ontvangen</h1>
         <p>Je hebt hier 2 opties:</p>
-        <Svg />
+        <span className={styles.vertical_line}></span>
+        <div className={ styles.option_container}>
+        <hr/>
+        <div className={styles.arrow_container}>
+          <ArrowRightAltIcon className={styles.arrowIconLeft}  />
+          <ArrowRightAltIcon className={styles.arrowIconRight} />
+        </div>
+        </div>
       </section>
       <hr/>
       <hr/>
