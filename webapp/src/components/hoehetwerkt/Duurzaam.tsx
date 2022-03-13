ee4eed20f7f2a3675f137ab2bc784550ef7f7a5e
Sven-27 <sven.notermans@live.nl>
2022-03-13 20:58:12 +0100

    still have to make duurzaam responsive.

--- webapp/src/components/hoehetwerkt/Duurzaam.tsx
+++ webapp/src/components/hoehetwerkt/Duurzaam.tsx
@@ -7,7 +7,10 @@ const Duurzaam = () => {
     kopStaartGroen, 
     lokaalGroen,
     plantaardigGroen, 
-    seizoenGroen 
+    seizoenGroen, 
+    menuLogo,
+    overGoedeten,
+    footerLogo
   } = Logos
 
   return (
@@ -22,6 +25,19 @@ const Duurzaam = () => {
       <p className={styles.text_options}>
         Aan de iconen bovenaan elk gerecht zie je aan welke 
         duurzaamheidspijler(s) het voldoet.
+      </p>
+      <figure className={styles.hamburgerContainer}>
+        <img className={styles.menuLogo} src={menuLogo} alt="menu-logo" />
+        <img className={styles.overGoedeten} src={overGoedeten} alt="goedeten-logo" />
+      </figure>
+      <p className={styles.menu}>
+        Ga via het menu rechtsboven naar 'over GoedEten'
+      </p>
+      <p className={styles.of}>of</p>
+      <img className={styles.footerLogo} src={footerLogo} alt="logo goedeten" />
+      <p className={styles.infoGoedEten}>
+        Klik linksonder op het logo om meer te lezen
+        over het GoedEten concept.
       </p>
     </div>
   )
