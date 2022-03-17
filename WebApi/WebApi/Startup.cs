bf430b0ffe22693608bd95fb1b433a158dd58559
BEE-International <f.vandergeld@bee-international.nl>
2022-03-17 11:08:43 +0100

    Fixed broken logging

--- WebApi/WebApi/Startup.cs
+++ WebApi/WebApi/Startup.cs
@@ -258,6 +258,7 @@ namespace WebApi
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
             //}
+            app.UseElmahIo();
 
             app.UseCors("CorsPolicy");
 
